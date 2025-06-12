04185ff5ac757b423eee51297f671f09bda78e00
Andrii Shpek <andrii.shpek@sigma.software>
2025-06-12 22:22:48 +0300

    Add koin initialization

--- app/src/main/java/com/testapp/audiobookplayer/AudiobookPlayerApp.kt
+++ app/src/main/java/com/testapp/audiobookplayer/AudiobookPlayerApp.kt
@@ -1,5 +1,19 @@
 package com.testapp.audiobookplayer
 
 import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 
-class AudiobookPlayerApp : Application()
+class AudiobookPlayerApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+            androidContext(this@AudiobookPlayerApp)
+            // TODO koin init modules
+        }
+    }
+}
