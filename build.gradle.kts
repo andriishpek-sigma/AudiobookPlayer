f5f445522035c6a2a57b1ab832dcbdbcf8b9460a
Andrii Shpek <andrii.shpek@sigma.software>
2025-06-12 21:58:25 +0300

    Add ktlint and editorconfig

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,28 @@
+import org.jlleitschuh.gradle.ktlint.KtlintExtension
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.ktlintGradle)
 }
+
+//region Ktlint configuration
+configure<KtlintExtension> {
+    configure()
+}
+
+subprojects {
+    apply(plugin = rootProject.libs.plugins.ktlintGradle.get().pluginId)
+
+    configure<KtlintExtension> {
+        configure()
+    }
+}
+
+fun KtlintExtension.configure() {
+    version.set("1.6.0")
+    android.set(true)
+}
+//endregion
