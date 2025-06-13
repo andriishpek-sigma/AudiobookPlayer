90cc4bc6c57694e8f8d56db869e5d3a305a5d88b
Andrii Shpek <andrii.shpek@sigma.software>
2025-06-13 11:50:19 +0300

    Add koin di tree verification unit test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
 
     testImplementation(libs.junit)
     testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.koin.test.android)
 
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
