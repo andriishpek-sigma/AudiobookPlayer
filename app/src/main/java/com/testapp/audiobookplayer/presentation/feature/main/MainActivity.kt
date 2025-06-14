32ad4898696c24ceb08b550778c8b46697d83a72
Andrii Shpek <andrii.shpek@sigma.software>
2025-06-14 11:58:07 +0300

    Move main activity to feature main package

--- app/src/main/java/com/testapp/audiobookplayer/presentation/feature/main/MainActivity.kt
+++ app/src/main/java/com/testapp/audiobookplayer/presentation/feature/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.testapp.audiobookplayer.presentation
+package com.testapp.audiobookplayer.presentation.feature.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
