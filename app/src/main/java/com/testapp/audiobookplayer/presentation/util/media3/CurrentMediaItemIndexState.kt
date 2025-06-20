ca1889e26dd1f39cdc8d3209c42145b1b1dada90
Andrii Shpek <andrii.shpek@sigma.software>
2025-06-20 12:47:30 +0300

    Update current media item index state to account for timeline availability

--- app/src/main/java/com/testapp/audiobookplayer/presentation/util/media3/CurrentMediaItemIndexState.kt
+++ app/src/main/java/com/testapp/audiobookplayer/presentation/util/media3/CurrentMediaItemIndexState.kt
@@ -3,7 +3,7 @@ package com.testapp.audiobookplayer.presentation.util.media3
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.media3.common.Player
@@ -20,7 +20,7 @@ class CurrentMediaItemIndexState(
     private val player: Player,
 ) {
 
-    var value by mutableIntStateOf(player.currentMediaItemIndex)
+    var value by mutableStateOf(resolveCurrentMediaItemIndex())
         private set
 
     suspend fun observe() {
@@ -28,10 +28,18 @@ class CurrentMediaItemIndexState(
             if (
                 events.containsAny(
                     Player.EVENT_MEDIA_ITEM_TRANSITION,
+                    Player.EVENT_AVAILABLE_COMMANDS_CHANGED,
                 )
             ) {
-                value = player.currentMediaItemIndex
+                value = resolveCurrentMediaItemIndex()
             }
         }
+    }
+
+    private fun resolveCurrentMediaItemIndex(): Int? {
+        if (!player.isCommandAvailable(Player.COMMAND_GET_TIMELINE)) {
+            return null
+        }
+        return player.currentMediaItemIndex
     }
 }
