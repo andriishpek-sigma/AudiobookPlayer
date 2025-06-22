60a53f5c601ae82c138f1a6600c327d12af2ac93
Andrii Shpek <andrii.shpek@sigma.software>
2025-06-22 17:37:33 +0300

    Implement book repository and mock provider

---
+++ app/src/main/java/com/testapp/audiobookplayer/data/feature/book/BookRepositoryImpl.kt
@@ -0,0 +1,29 @@
+package com.testapp.audiobookplayer.data.feature.book
+
+import com.testapp.audiobookplayer.domain.feature.book.model.Book
+import com.testapp.audiobookplayer.domain.feature.book.model.BookChapter
+import com.testapp.audiobookplayer.domain.feature.book.model.BookId
+import com.testapp.audiobookplayer.domain.feature.book.repository.BookRepository
+import org.koin.core.annotation.Single
+import kotlinx.coroutines.delay
+
+// Ideally should be a 'reusable' factory (repo should be stateless), but koin doesn't support that
+@Single
+class BookRepositoryImpl(
+    private val bookMockProvider: BookMockProvider,
+) : BookRepository {
+
+    override suspend fun getBook(id: BookId): Result<Book> {
+        delay(500)
+        return Result.success(
+            bookMockProvider.provideMockedBook(id),
+        )
+    }
+
+    override suspend fun getBookChapters(id: BookId): Result<List<BookChapter>> {
+        delay(1000)
+        return Result.success(
+            bookMockProvider.provideMockedChapters(id),
+        )
+    }
+}
---
+++ app/src/main/java/com/testapp/audiobookplayer/domain/feature/book/repository/BookRepository.kt
@@ -0,0 +1,12 @@
+package com.testapp.audiobookplayer.domain.feature.book.repository
+
+import com.testapp.audiobookplayer.domain.feature.book.model.Book
+import com.testapp.audiobookplayer.domain.feature.book.model.BookChapter
+import com.testapp.audiobookplayer.domain.feature.book.model.BookId
+
+interface BookRepository {
+
+    suspend fun getBook(id: BookId): Result<Book>
+
+    suspend fun getBookChapters(id: BookId): Result<List<BookChapter>>
+}
