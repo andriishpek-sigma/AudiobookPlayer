3890ae30f246dcaf36a1424109cc3b552fc1f5f4
Andrii Shpek <andrii.shpek@sigma.software>
2025-06-22 18:04:54 +0300

    Move application class to a separate package

--- app/src/main/java/com/testapp/audiobookplayer/app/AudiobookPlayerApp.kt
+++ app/src/main/java/com/testapp/audiobookplayer/app/AudiobookPlayerApp.kt
@@ -1,6 +1,7 @@
-package com.testapp.audiobookplayer
+package com.testapp.audiobookplayer.app
 
 import android.app.Application
+import com.testapp.audiobookplayer.AudiobookPlayerModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
