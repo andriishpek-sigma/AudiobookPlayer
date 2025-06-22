bebf801cd998bd557d9bbb1c023af6bb3e591691
Andrii Shpek <andrii.shpek@sigma.software>
2025-06-22 14:25:06 +0300

    Rework button controls in size

--- app/src/main/java/com/testapp/audiobookplayer/presentation/feature/audiobook/screen/AudiobookPlayerButtonControls.kt
+++ app/src/main/java/com/testapp/audiobookplayer/presentation/feature/audiobook/screen/AudiobookPlayerButtonControls.kt
@@ -5,9 +5,14 @@ package com.testapp.audiobookplayer.presentation.feature.audiobook.screen
 import androidx.annotation.OptIn
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Forward10
 import androidx.compose.material.icons.rounded.Pause
@@ -15,19 +20,23 @@ import androidx.compose.material.icons.rounded.PlayArrow
 import androidx.compose.material.icons.rounded.Replay5
 import androidx.compose.material.icons.rounded.SkipNext
 import androidx.compose.material.icons.rounded.SkipPrevious
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.session.MediaController
@@ -86,6 +95,7 @@ private fun PlayPauseButton(
         // TODO maybe progress indicator if disabled?
         enabled = playPauseState?.isEnabled == true,
         image = if (isPlaying) Icons.Rounded.Pause else Icons.Rounded.PlayArrow,
+        imagePadding = 4.dp,
         contentDescriptionRes = if (isPlaying) {
             R.string.player_pause_description
         } else {
@@ -106,6 +116,7 @@ private fun ReplayButton(
         },
         enabled = true,
         image = Icons.Rounded.Replay5,
+        imagePadding = 12.dp,
         contentDescriptionRes = R.string.player_replay_5_description,
     )
 }
@@ -122,6 +133,7 @@ private fun ForwardButton(
         },
         enabled = true,
         image = Icons.Rounded.Forward10,
+        imagePadding = 12.dp,
         contentDescriptionRes = R.string.player_forward_10_description,
     )
 }
@@ -142,6 +154,7 @@ private fun PreviousTrackButton(
         },
         enabled = previousButtonState?.isEnabled == true,
         image = Icons.Rounded.SkipPrevious,
+        imagePadding = 12.dp,
         contentDescriptionRes = R.string.player_skip_previous_description,
     )
 }
@@ -162,6 +175,7 @@ private fun NextTrackButton(
         },
         enabled = nextButtonState?.isEnabled == true,
         image = Icons.Rounded.SkipNext,
+        imagePadding = 12.dp,
         contentDescriptionRes = R.string.player_skip_next_description,
     )
 }
@@ -171,24 +185,37 @@ private fun PlayerControlButton(
     onClick: () -> Unit,
     enabled: Boolean,
     image: ImageVector,
+    imagePadding: Dp,
     @StringRes contentDescriptionRes: Int,
     modifier: Modifier = Modifier,
 ) {
     val haptic = LocalHapticFeedback.current
 
-    IconButton(
-        modifier = modifier,
-        onClick = {
-            onClick()
-            haptic.performHapticFeedback(HapticFeedbackType.Confirm)
-        },
-        enabled = enabled,
+    // Custom implementation of IconButton to fine tune icon sizes
+    Box(
+        modifier = modifier
+            .size(64.dp)
+            .minimumInteractiveComponentSize()
+            .clip(CircleShape)
+            .clickable(
+                onClick = {
+                    onClick()
+                    haptic.performHapticFeedback(HapticFeedbackType.Confirm)
+                },
+                enabled = enabled,
+                role = Role.Button,
+            )
+            .padding(imagePadding),
+        contentAlignment = Alignment.Center,
     ) {
+        val enabledColor = LocalContentColor.current
+        val currentColor = if (enabled) enabledColor else enabledColor.copy(alpha = 0.4f)
+
         Image(
             modifier = Modifier.fillMaxSize(),
             imageVector = image,
             contentDescription = stringResource(contentDescriptionRes),
-            colorFilter = ColorFilter.tint(LocalContentColor.current),
+            colorFilter = ColorFilter.tint(currentColor),
         )
     }
 }
