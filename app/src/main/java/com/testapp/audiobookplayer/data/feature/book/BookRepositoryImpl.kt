43462c37841dbcd3c960130f2cec8ebccfa9c7fe
Andrii Shpek <andrii.shpek@sigma.software>
2025-06-22 18:06:53 +0300

    Run repository methods on ui dispatcher

--- app/src/main/java/com/testapp/audiobookplayer/data/feature/book/BookRepositoryImpl.kt
+++ app/src/main/java/com/testapp/audiobookplayer/data/feature/book/BookRepositoryImpl.kt
@@ -1,28 +1,37 @@
 package com.testapp.audiobookplayer.data.feature.book
 
+import com.testapp.audiobookplayer.app.DispatcherType
 import com.testapp.audiobookplayer.domain.feature.book.model.Book
 import com.testapp.audiobookplayer.domain.feature.book.model.BookChapter
 import com.testapp.audiobookplayer.domain.feature.book.model.BookId
 import com.testapp.audiobookplayer.domain.feature.book.repository.BookRepository
 import org.koin.core.annotation.Single
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 
 // Ideally should be a 'reusable' factory (repo should be stateless), but koin doesn't support that
 @Single
 class BookRepositoryImpl(
     private val bookMockProvider: BookMockProvider,
+    @DispatcherType.IO
+    private val ioDispatcher: CoroutineDispatcher,
 ) : BookRepository {
 
-    override suspend fun getBook(id: BookId): Result<Book> {
+    override suspend fun getBook(
+        id: BookId,
+    ): Result<Book> = withContext(ioDispatcher) {
         delay(500)
-        return Result.success(
+        Result.success(
             bookMockProvider.provideMockedBook(id),
         )
     }
 
-    override suspend fun getBookChapters(bookId: BookId): Result<List<BookChapter>> {
+    override suspend fun getBookChapters(
+        bookId: BookId,
+    ): Result<List<BookChapter>> = withContext(ioDispatcher) {
         delay(1000)
-        return Result.success(
+        Result.success(
             bookMockProvider.provideMockedChapters(bookId),
         )
     }
