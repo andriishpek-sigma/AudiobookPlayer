6e0a4819c80969234297a17c6ac23c0e73a346be
Andrii Shpek <andrii.shpek@sigma.software>
2025-06-23 11:16:33 +0300

    Cleanup media items mapping

--- app/src/main/java/com/testapp/audiobookplayer/presentation/feature/audiobook/AudiobookPlayerScreen.kt
+++ app/src/main/java/com/testapp/audiobookplayer/presentation/feature/audiobook/AudiobookPlayerScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.core.net.toUri
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -18,7 +19,7 @@ import androidx.media3.session.SessionError
 import com.testapp.audiobookplayer.domain.feature.book.model.BookId
 import com.testapp.audiobookplayer.presentation.feature.audiobook.screen.AudiobookPlayerScreenContent
 import com.testapp.audiobookplayer.presentation.feature.player.AudiobookPlayerService
-import com.testapp.audiobookplayer.presentation.mvi.ConsumeEffects
+import com.testapp.audiobookplayer.presentation.util.asUiList
 import com.testapp.audiobookplayer.presentation.util.media3.rememberMediaControllerStateWithLifecycle
 import org.koin.androidx.compose.koinViewModel
 import org.koin.core.parameter.parametersOf
@@ -66,14 +67,16 @@ private fun StartAudiobookPlaybackWhenLoaded(
 
     val audiobookMediaController = audiobookMediaControllerState.value ?: return
 
-    LaunchedEffect(audiobookMediaController, book, chapters) {
-        val mediaItems = chapters.map {
+    val mediaItems = remember(book, chapters) {
+        chapters.map {
             createMediaItem(
                 book = book,
                 chapter = it,
             )
-        }
+        }.asUiList()
+    }
 
+    LaunchedEffect(audiobookMediaController, mediaItems) {
         audiobookMediaController.setMediaItems(mediaItems)
         audiobookMediaController.repeatMode = Player.REPEAT_MODE_OFF
         audiobookMediaController.prepare()
